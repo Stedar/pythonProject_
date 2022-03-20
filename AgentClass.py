513adf863b54446ae6db5357d2b933594a073c8c
S. Beloyar <velemir.s@gmail.com>
2022-03-20 13:43:20 +0300

    реалзован random bot

---
+++ AgentClass.py
@@ -0,0 +1,9 @@
+#абсртактный класс для общих методов
+from BoardClass import Board
+
+class Agent():
+    def __init__(self,board):
+        self.board = board
+
+    def make_turn(self,side):
+        pass
